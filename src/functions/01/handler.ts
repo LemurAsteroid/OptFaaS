cde9211a321316cc8685b8790398c8e145efb2ac
Arne <arne.paw@gmail.com>
2023-09-23 11:25:42 +0200

    [add] test function

--- src/functions/01/handler.ts
+++ src/functions/01/handler.ts
@@ -1,13 +1,12 @@
-import { now } from 'performance-now'
-import { fs } from 'fs'
+// import { now } from 'performance-now'
+// import { fs } from 'fs'
 
+export const matrixMultiplication = async (event, context, callback) => {
 
-const matrixMultiplication = async (event, context, callback) => {
-
-  let cpuinfo = fs.readFileSync('/proc/cpuinfo', 'utf8');
+  /* let cpuinfo = fs.readFileSync('/proc/cpuinfo', 'utf8');
   let instanceId = fs.readFileSync('/proc/self/cgroup', 'utf-8');
   let meminfo = fs.readFileSync('/proc/meminfo', 'utf8');
-  let uptime = fs.readFileSync('/proc/uptime', 'utf-8');
+  let uptime = fs.readFileSync('/proc/uptime', 'utf-8'); */
 
   let n;
 
@@ -17,7 +16,7 @@ const matrixMultiplication = async (event, context, callback) => {
       n = 100;
   }
 
-  let [initTime, procTime, result] = matrix(n);
+  matrix(n);
 
   const res = {
     statusCode: 200,
@@ -29,16 +28,7 @@ const matrixMultiplication = async (event, context, callback) => {
         payload: {
             'test': 'matrix test',
             'n': Number(n),
-            'initTime': Number((initTime).toFixed(3)),
-            'procTime': Number((procTime).toFixed(3))
             
-        },
-        metrics: {
-            machineid: '',
-            instanceid: instanceId,
-            cpu: cpuinfo,
-            mem: meminfo,
-            uptime: uptime
         }
     })
   };
@@ -50,14 +40,14 @@ const randomTable = (size) => Array.from(
   () => Array.from({length: size}, () => Math.floor(Math.random() * 100))
 )
 
-function matrix(n: number): [number, number, any] {
-  let startInit = now();
+function matrix(n: number) {
+  
   const matrixA = randomTable(n);
   const matrixB = randomTable(n);
-  let endInit = now();
+  
 
   const matrixMult: any[] = [];
-  let startProc = now();
+  
   for (let i = 0; i < matrixA.length; i++) {
     matrixMult[i] = [];
     for (let j = 0; j < matrixB.length; j++) {
@@ -68,9 +58,7 @@ function matrix(n: number): [number, number, any] {
       matrixMult[i][j] = sum;
     }
   }
-  let endProc = now();
+  
 
-  return [(endInit - startInit), (endProc - startProc), matrixMult];
+  return true;
 }
-
-export const main = matrixMultiplication
---
+++ src/functions/01/index.ts
@@ -0,0 +1,4 @@
+export default {
+    handler: './src/functions/01/Nodejs/handler.main',
+    memorySize: 256
+};
