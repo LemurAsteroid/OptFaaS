3f110876dfcc06a7c8910a03824aed1d59b8b6dd
Arne <arne.paw@gmail.com>
2023-09-23 11:19:53 +0200

    add constants

--- variables.ts
+++ variables.ts
@@ -1,7 +1,28 @@
-const variables = {
-    REGION: ['us-east-1'],
-    SCHEDULED_FUNCTIONS: ["02"],
-    NUMBER_OF_PARALLELIZATION: 10,
+export const variables = {
+    REGION: ['us'],
+    SCHEDULED_FUNCTIONS: ["01", "02"],
+    NUMBER_OF_PARALLELIZATION: 5,
+    CLOUD_WATCH_TRIGGER_ARN: "",
+    GCP_BENCHMARK_RUNNER_URL: "https://us-central1-optfaas.cloudfunctions.net/benchmarkRunner",
 } as const;
+
+export const AWS_REGIONS: { [key: string]: string } = {
+    "us": "us-east-1",
+    "asia": "ap-northeast-1",
+    "eu": "eu-central-1"
+}
+
+export const GCP_REGIONS: { [key: string]: string } = {
+    "us": "us-central1"
+}
+
+
+export const URL_DICT_GCF: { [key: string]: string } = {
+    "us": "https://us-central1-optfaas.cloudfunctions.net/",
+};
+
+export const URL_DICT_AWS: { [key: string]: string } = {
+    "us": "https://us-central1-optfaas.cloudfunctions.net/",
+};
 
 export default variables;
