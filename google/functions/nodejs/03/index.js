301bfe51b6938f2b1eddcab31577531b6a8457e5
Arne Pawlowski <arne.pawlowski@compax.at>
2023-11-04 15:50:47 +0100

    [add] benchmark function 03 GCP

---
+++ google/functions/nodejs/03/index.js
@@ -0,0 +1,53 @@
+const path = require('path');
+const { spawn } = require('child_process');
+const os = require('os');
+
+const { Storage } = require('@google-cloud/storage');
+
+const ffmpegPath = require('@ffmpeg-installer/ffmpeg').path;
+
+const ffmpeg = ffmpegPath;
+const storage = new Storage();
+
+const BUCKET_NAME = 'video_benchmark_bucket';
+const VIDEO_NAME = 'benchmark_sample_video.mp4';
+
+
+exports.convertVideoToGif = async (event, context) => {
+    const tmpDir = os.tmpdir();
+    const file = storage.bucket(BUCKET_NAME).file(VIDEO_NAME);
+    const tempLocalFile = path.join(tmpDir, VIDEO_NAME);
+    const tempLocalGif = path.join(tmpDir, `${path.basename(VIDEO_NAME, path.extname(VIDEO_NAME))}.gif`);
+
+    // Download the video from GCP to a temporary directory
+    await file.download({ destination: tempLocalFile });
+
+    // Convert the video to GIF using FFmpeg
+    const ffmpegProcess = spawn(ffmpeg, [
+        '-i', tempLocalFile,
+        '-vf', 'fps=10,scale=320:-1:flags=lanczos',
+        tempLocalGif,
+    ]);
+
+    return new Promise((resolve, reject) => {
+        ffmpegProcess.on('close', (code) => {
+            if (code === 0) {
+                // Upload the GIF to GCP
+                storage.bucket(BUCKET_NAME).upload(tempLocalGif, {
+                    destination: `converted/${path.basename(tempLocalGif)}`,
+                }, (err) => {
+                    if (err) {
+                        console.error('Error uploading GIF:', err);
+                        reject(err);
+                    } else {
+                        console.log('GIF uploaded successfully.');
+                        resolve();
+                    }
+                });
+            } else {
+                console.error('FFmpeg process failed with code ' + code);
+                reject(new Error('FFmpeg process failed'));
+            }
+        });
+    });
+};
